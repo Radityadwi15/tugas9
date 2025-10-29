193b9c4f1e499438306e55153863e92b330747e7
Radityadwi15 <akuradit6429@gmail.com>
2025-10-29 10:36:40 +0700

    komitan 2

--- koneksi.php
+++ koneksi.php
@@ -3,13 +3,15 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 $host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "db_xirpl1_27_1"; // <== pastikan sama persis
+$user = "xirpl1_27";   // ganti sesuai username MySQL yang kamu dapat dari hosting
+$pass = "password_kamu"; // ganti dengan password MySQL kamu
+$db   = "db_xirpl1_27_1";
 
 $conn = mysqli_connect($host, $user, $pass, $db);
 
 if (!$conn) {
   die("Koneksi gagal: " . mysqli_connect_error());
+} else {
+  echo "Koneksi berhasil!";
 }
 ?>
